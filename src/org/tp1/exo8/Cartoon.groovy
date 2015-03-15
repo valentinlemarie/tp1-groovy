aac9368830a958c23b89c241fee0df8ca5aa3917
raclet <raclet@irit.fr>
2015-03-15 06:05:59 +0100

    exo 1 à 8

---
+++ src/org/tp1/exo8/Cartoon.groovy
@@ -0,0 +1,9 @@
+package org.tp1.exo8
+
+/**
+ * Created by raclet on 15/03/2015.
+ */
+class Cartoon {
+    String name
+    Feeling feeling
+}
---
+++ src/org/tp1/exo8/Feeling.groovy
@@ -0,0 +1,14 @@
+package org.tp1.exo8
+
+/**
+ * Created by raclet on 15/03/2015.
+ */
+public enum Feeling {
+    Happy, Sad, Neutral, Suicidal, Anticipation, Surprised, Relaxed, Guilty
+
+    // ------------ START EDITING HERE ----------------------
+
+    // ------------ STOP EDITING HERE  ----------------------
+
+
+}
